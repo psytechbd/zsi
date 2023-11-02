ce4d23ed2903835c567e37572360bddf82d30b48
unknown <psytechbd@gmail.com>
2023-11-03 00:54:16 +0600

    Top part Banner work

---
+++ js/script.js
@@ -0,0 +1,22 @@
+jQuery(document).ready(function($) {
+    $('.membership-include').owlCarousel({
+        margin: 10,
+        nav: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        },
+        animateOut: 'fadeOut',
+        autoplay: true,
+        autoplayTimeout: 8000,
+        autoplayHoverPause: true,
+        loop: true
+    });
+});
